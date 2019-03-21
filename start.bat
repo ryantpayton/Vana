bb6ca98e6d03c28fdf3a47995ff64c4d179ca1ca
Ryan Payton <ryantpaytong@gmail.com>
2019-03-21 01:55:00 -0400

    Fixed start batch file

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
-start "LoginServer" Debug\LoginServer\LoginServer.exe
+start "LoginServer" Debug_VC140\LoginServer\LoginServer.exe
 ping 127.0.0.1 > nul
-start "WorldServer0" Debug\WorldServer\WorldServer.exe
-start "channelServer0" Debug\ChannelServer\ChannelServer.exe
+start "WorldServer0" Debug_VC140\WorldServer\WorldServer.exe
+start "channelServer0" Debug_VC140\ChannelServer\ChannelServer.exe
